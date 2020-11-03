ca627f3f21ecee04cde2e4d5e26ec422aa361eb6
Bella Thapapong <thapapong.sethpitak@thoughtworks.com>
2020-11-03 12:50:06 +0700

    Add PageView implementatiom

--- app/src/main/java/com/example/helloworldshopping/MyOfferActivity.java
+++ app/src/main/java/com/example/helloworldshopping/MyOfferActivity.java
@@ -1,14 +1,22 @@
 package com.example.helloworldshopping;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.viewpager.widget.ViewPager;
 
-public class MyOfferActivity extends AppCompatActivity {
+import com.example.helloworldshopping.adapter.MyPageAdapter;
+
+
+public class MyOfferActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_offer);
+
+        MyPageAdapter myPageAdapter = new MyPageAdapter(getSupportFragmentManager());
+        final ViewPager pager = findViewById(R.id.viewPager);
+        pager.setAdapter(myPageAdapter);
+        pager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener());
+
     }
 }
---
+++ app/src/main/java/com/example/helloworldshopping/adapter/MyPageAdapter.java
@@ -0,0 +1,36 @@
+package com.example.helloworldshopping.adapter;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.helloworldshopping.Offer1Fragment;
+import com.example.helloworldshopping.Offer2Fragment;
+import com.example.helloworldshopping.Offer3Fragment;
+
+public class MyPageAdapter extends FragmentPagerAdapter {
+
+    public MyPageAdapter(FragmentManager fragMan) {
+        super(fragMan);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return Offer1Fragment.newInstance(null, null);
+            case 1:
+                return Offer2Fragment.newInstance(null, null);
+            case 2:
+                return Offer3Fragment.newInstance(null, null);
+            default:
+                return null;
+        }
+    }
+}
